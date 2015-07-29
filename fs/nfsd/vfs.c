03c345a4d36ec18764ab7ccc7c709c361d20ca61
Andreas Gruenbacher <agruenba@redhat.com>
2015-07-29 14:49:31 +0200

    nfsd: Add support for the MAY_CREATE_{FILE,DIR} permissions
    For local file systems, the vfs performs the necessary permission checks
    for operations like creating files and directories.  NFSd duplicates
    several of those checks.  The vfs checks have been extended to check for
    additional permissions like MAY_CREATE_FILE and MY_CREATE_DIR; the nfsd
    checks currently lack those extensions.
    
    Ideally, all duplicate checks should be removed; for now, just fix the
    duplicate checks instead though.
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    Acked-by: J. Bruce Fields <bfields@redhat.com>
    
    BACKPORT Conflicts:
    	fs/nfsd/vfs.c
    
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfsd/vfs.c
+++ fs/nfsd/vfs.c
@@ -1214,6 +1214,8 @@ nfsd_create_locked(struct svc_rqst *rqstp, struct svc_fh *fhp,
 	__be32		err;
 	__be32		err2;
 	int		host_err;
+	int access = (type == S_IFDIR) ?
+		NFSD_MAY_CREATE_DIR : NFSD_MAY_CREATE_FILE;
 
 	dentry = fhp->fh_dentry;
 	dirp = d_inode(dentry);
@@ -1226,7 +1228,7 @@ nfsd_create_locked(struct svc_rqst *rqstp, struct svc_fh *fhp,
 		goto out;
 	}
 
-	err = nfsd_permission(rqstp, fhp->fh_export, dentry, NFSD_MAY_CREATE);
+	err = nfsd_permission(rqstp, fhp->fh_export, dentry, access);
 	if (err)
 		goto out;
 
@@ -1403,7 +1405,7 @@ do_nfsd_create(struct svc_rqst *rqstp, struct svc_fh *fhp,
 
 	/* If file doesn't exist, check for permissions to create one */
 	if (d_really_is_negative(dchild)) {
-		err = fh_verify(rqstp, fhp, S_IFDIR, NFSD_MAY_CREATE);
+		err = fh_verify(rqstp, fhp, S_IFDIR, NFSD_MAY_CREATE_FILE);
 		if (err)
 			goto out;
 	}
@@ -1582,7 +1584,7 @@ nfsd_symlink(struct svc_rqst *rqstp, struct svc_fh *fhp,
 	if (isdotent(fname, flen))
 		goto out;
 
-	err = fh_verify(rqstp, fhp, S_IFDIR, NFSD_MAY_CREATE);
+	err = fh_verify(rqstp, fhp, S_IFDIR, NFSD_MAY_CREATE_FILE);
 	if (err)
 		goto out;
 
@@ -1629,7 +1631,7 @@ nfsd_link(struct svc_rqst *rqstp, struct svc_fh *ffhp,
 	__be32		err;
 	int		host_err;
 
-	err = fh_verify(rqstp, ffhp, S_IFDIR, NFSD_MAY_CREATE);
+	err = fh_verify(rqstp, ffhp, S_IFDIR, NFSD_MAY_CREATE_FILE);
 	if (err)
 		goto out;
 	err = fh_verify(rqstp, tfhp, 0, NFSD_MAY_NOP);
@@ -1723,11 +1725,12 @@ nfsd_rename(struct svc_rqst *rqstp, struct svc_fh *ffhp, char *fname, int flen,
 	__be32		err;
 	int		host_err;
 	bool		close_cached = false;
+	int		access;
 
 	err = fh_verify(rqstp, ffhp, S_IFDIR, NFSD_MAY_REMOVE);
 	if (err)
 		goto out;
-	err = fh_verify(rqstp, tfhp, S_IFDIR, NFSD_MAY_CREATE);
+	err = fh_verify(rqstp, tfhp, S_IFDIR, NFSD_MAY_NOP);
 	if (err)
 		goto out;
 
@@ -1765,6 +1768,13 @@ retry:
 		goto out_dput_old;
 	host_err = -EINVAL;
 	if (odentry == trap)
+		goto out_dput_old;
+
+	host_err = 0;
+	access = S_ISDIR(d_inode(odentry)->i_mode) ?
+		NFSD_MAY_CREATE_DIR : NFSD_MAY_CREATE_FILE;
+	err = fh_verify(rqstp, tfhp, S_IFDIR, access);
+	if (err)
 		goto out_dput_old;
 
 	ndentry = lookup_one_len(tname, tdentry, tlen);
@@ -1806,7 +1816,8 @@ retry:
  out_dput_old:
 	dput(odentry);
  out_nfserr:
-	err = nfserrno(host_err);
+	if (host_err)
+		err = nfserrno(host_err);
 	/*
 	 * We cannot rely on fh_unlock on the two filehandles,
 	 * as that would do the wrong thing if the two directories
@@ -2396,9 +2407,10 @@ nfsd_permission(struct svc_rqst *rqstp, struct svc_export *exp,
 	    uid_eq(inode->i_uid, current_fsuid()))
 		return 0;
 
-	/* This assumes  NFSD_MAY_{READ,WRITE,EXEC} == MAY_{READ,WRITE,EXEC} */
+	/* This assumes NFSD_MAY_{READ,WRITE,EXEC} == MAY_{READ,WRITE,EXEC}. */
 	err = inode_permission(&init_user_ns, inode,
-			       acc & (MAY_READ | MAY_WRITE | MAY_EXEC));
+			       acc & (MAY_READ | MAY_WRITE | MAY_EXEC |
+				      MAY_CREATE_DIR | MAY_CREATE_FILE));
 
 	/* Allow read access to binaries even when mode 111 */
 	if (err == -EACCES && S_ISREG(inode->i_mode) &&
