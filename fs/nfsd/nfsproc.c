7d3cdaf318c6a5be819bd98bb7dbfadcca65b5a5
Jeff Layton <jeff.layton@primarydata.com>
2015-08-08 06:23:14 -0400

    nfsd: PD-9518 - retry once in nfsd_open on an -EOPENSTALE return
    If we get back -EOPENSTALE from an NFSv4 open, then we either got some
    unhandled error or the inode we got back was not the same as the one
    associated with the dentry.
    
    We really have no recourse in that situation other than to retry the
    open, and if it fails to just return nfserr_stale back to the client.
    
    Signed-off-by: Jeff Layton <jeff.layton@primarydata.com>
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>

--- fs/nfsd/nfsproc.c
+++ fs/nfsd/nfsproc.c
@@ -809,6 +809,7 @@ nfserrno (int errno)
 		{ nfserr_serverfault, -ESERVERFAULT },
 		{ nfserr_serverfault, -ENFILE },
 		{ nfserr_io, -EREMOTEIO },
+		{ nfserr_stale, -EOPENSTALE },
 		{ nfserr_io, -EUCLEAN },
 		{ nfserr_perm, -ENOKEY },
 	};
