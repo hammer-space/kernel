b611ce1f4c2f96bd485267ef1b80071ea64c177d
Peng Tao <tao.peng@primarydata.com>
2015-11-04 10:15:29 +0800

    nfs/flexfiles: use sunrpc help to encode ipv6 addr
    Signed-off-by: Peng Tao <tao.peng@primarydata.com>
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>

--- fs/nfs/flexfilelayout/flexfilelayout.c
+++ fs/nfs/flexfilelayout/flexfilelayout.c
@@ -13,6 +13,7 @@
 #include <linux/sched/mm.h>
 
 #include <linux/sunrpc/metrics.h>
+#include <linux/sunrpc/addr.h>
 
 #include "flexfilelayout.h"
 #include "../nfs4session.h"
@@ -2232,36 +2233,7 @@ ff_layout_ntop6_noscopeid(const struct sockaddr *sap, char *buf,
 	const struct sockaddr_in6 *sin6 = (struct sockaddr_in6 *)sap;
 	const struct in6_addr *addr = &sin6->sin6_addr;
 
-	/*
-	 * RFC 4291, Section 2.2.2
-	 *
-	 * Shorthanded ANY address
-	 */
-	if (ipv6_addr_any(addr))
-		return snprintf(buf, buflen, "::");
-
-	/*
-	 * RFC 4291, Section 2.2.2
-	 *
-	 * Shorthanded loopback address
-	 */
-	if (ipv6_addr_loopback(addr))
-		return snprintf(buf, buflen, "::1");
-
-	/*
-	 * RFC 4291, Section 2.2.3
-	 *
-	 * Special presentation address format for mapped v4
-	 * addresses.
-	 */
-	if (ipv6_addr_v4mapped(addr))
-		return snprintf(buf, buflen, "::ffff:%pI4",
-					&addr->s6_addr32[3]);
-
-	/*
-	 * RFC 4291, Section 2.2.1
-	 */
-	return snprintf(buf, buflen, "%pI6c", addr);
+	return rpc_ntop6_addr_noscopeid(addr, buf, buflen);
 }
 
 /* Derived from rpc_sockaddr2uaddr */
