f4829cf37037ae14ab04c58f3487cbe6fbd56043
Jeff Layton <jeff.layton@primarydata.com>
2016-04-14 11:15:58 -0400

    nfs: fix local_file pointer handling
    Signed-off-by: Jeff Layton <jeff.layton@primarydata.com>
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>

--- fs/nfs/flexfilelayout/flexfilelayout.c
+++ fs/nfs/flexfilelayout/flexfilelayout.c
@@ -228,7 +228,7 @@ static void ff_layout_free_mirror(struct nfs4_ff_layout_mirror *mirror)
 {
 	const struct cred	*cred;
 
-	if (!IS_ERR_OR_NULL(mirror->local_file))
+	if (mirror->local_file)
 		fput(mirror->local_file);
 	ff_layout_remove_mirror(mirror);
 	kfree(mirror->fh_versions);
