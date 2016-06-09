c250b7d6e394d05af0c6b4102f4db464b4ba7e3f
Trond Myklebust <trond.myklebust@primarydata.com>
2016-06-09 10:32:00 -0400

    NFS: Ensure we set the NFS_ATTR_FATTR_* flags for hidden+system+archive
    Otherwise we end up never clearing the NFS_INO_INVALID_ATTR flag in
    nfs_update_inode().
    
    Also fix ordering of decode_attr_hidden and decode_attr_fs_locations.
    Finally, note that time_create, hidden, system, archive and time_backup
    are supported in NFSv4 only.
    
    Yes, this should really be 3 patches.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>

--- fs/nfs/nfs4xdr.c
+++ fs/nfs/nfs4xdr.c
@@ -4145,6 +4145,7 @@ static int decode_attr_time_create(struct xdr_stream *xdr, uint32_t *bitmap, str
 
 static int decode_attr_hidden(struct xdr_stream *xdr, uint32_t *bitmap, uint32_t *res)
 {
+	int status = 0;
 	__be32 *p;
 
 	*res = 0;
@@ -4158,13 +4159,15 @@ static int decode_attr_hidden(struct xdr_stream *xdr, uint32_t *bitmap, uint32_t
 			*res |= NFS_HSA_HIDDEN;
 		}
 		bitmap[0] &= ~FATTR4_WORD0_HIDDEN;
+		status = NFS_ATTR_FATTR_HIDDEN;
 	}
 	dprintk("%s: hidden file: =%s\n", __func__, (*res & NFS_HSA_HIDDEN) == 0 ? "false" : "true");
-	return 0;
+	return status;
 }
 
 static int decode_attr_system(struct xdr_stream *xdr, uint32_t *bitmap, uint32_t *res)
 {
+	int status = 0;
 	__be32 *p;
 
 	*res = 0;
@@ -4178,9 +4181,10 @@ static int decode_attr_system(struct xdr_stream *xdr, uint32_t *bitmap, uint32_t
 			*res |= NFS_HSA_SYSTEM;
 		}
 		bitmap[1] &= ~FATTR4_WORD1_SYSTEM;
+		status = NFS_ATTR_FATTR_SYSTEM;
 	}
 	dprintk("%s: system file: =%s\n", __func__, (*res & NFS_HSA_HIDDEN) == 0 ? "false" : "true");
-	return 0;
+	return status;
 }
 
 static int decode_attr_time_backup(struct xdr_stream *xdr, uint32_t *bitmap, struct timespec *time)
@@ -4203,6 +4207,7 @@ static int decode_attr_time_backup(struct xdr_stream *xdr, uint32_t *bitmap, str
 
 static int decode_attr_archive(struct xdr_stream *xdr, uint32_t *bitmap, uint32_t *res)
 {
+	int status = 0;
 	__be32 *p;
 
 	*res = 0;
@@ -4216,9 +4221,10 @@ static int decode_attr_archive(struct xdr_stream *xdr, uint32_t *bitmap, uint32_
 			*res |= NFS_HSA_ARCHIVE;
 		}
 		bitmap[0] &= ~FATTR4_WORD0_ARCHIVE;
+		status = NFS_ATTR_FATTR_ARCHIVE;
 	}
 	dprintk("%s: archive file: =%s\n", __func__, (*res & NFS_HSA_ARCHIVE) == 0 ? "false" : "true");
-	return 0;
+	return status;
 }
 
 static int decode_attr_time_metadata(struct xdr_stream *xdr, uint32_t *bitmap, struct timespec *time)
@@ -4709,12 +4715,12 @@ static int decode_getfattr_attrs(struct xdr_stream *xdr, uint32_t *bitmap,
 		goto xdr_error;
 	fattr->valid |= status;
 
-	status = decode_attr_hidden(xdr, bitmap, &fattr->hsa_flags);
+	status = decode_attr_fs_locations(xdr, bitmap, fs_loc);
 	if (status < 0)
 		goto xdr_error;
 	fattr->valid |= status;
 
-	status = decode_attr_fs_locations(xdr, bitmap, fs_loc);
+	status = decode_attr_hidden(xdr, bitmap, &fattr->hsa_flags);
 	if (status < 0)
 		goto xdr_error;
 	fattr->valid |= status;
