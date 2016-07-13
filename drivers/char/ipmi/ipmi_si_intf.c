f0dab1bc81a33734e3e92e55c40b17e30f8d34be
Trond Myklebust <trond.myklebust@primarydata.com>
2016-07-13 17:36:02 -0400

    Revert "ipmi: move timer init to before irq is setup"
    This reverts commit 69bed67bc32fd47774539eaddb9f33420df556fe.
    
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>

--- drivers/char/ipmi/ipmi_si_intf.c
+++ drivers/char/ipmi/ipmi_si_intf.c
@@ -1142,16 +1142,16 @@ static int smi_start_processing(void            *send_info,
 
 	new_smi->intf = intf;
 
-	/* Set up the timer that drives the interface. */
-	timer_setup(&new_smi->si_timer, smi_timeout, 0);
-	new_smi->timer_can_start = true;
-	smi_mod_timer(new_smi, jiffies + SI_TIMEOUT_JIFFIES);
-
 	/* Try to claim any interrupts. */
 	if (new_smi->io.irq_setup) {
 		new_smi->io.irq_handler_data = new_smi;
 		new_smi->io.irq_setup(&new_smi->io);
 	}
+
+	/* Set up the timer that drives the interface. */
+	timer_setup(&new_smi->si_timer, smi_timeout, 0);
+	new_smi->timer_can_start = true;
+	smi_mod_timer(new_smi, jiffies + SI_TIMEOUT_JIFFIES);
 
 	/*
 	 * Check if the user forcefully enabled the daemon.
