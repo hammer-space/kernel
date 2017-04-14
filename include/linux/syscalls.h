7d7727e0eb95d3061bfb7b21d512c2f7a9893907
Trond Myklebust <trond.myklebust@primarydata.com>
2017-04-14 12:46:28 -0400

    Add mkdirat2() syscall to allow passing of a flag argument
    Add a syscall mkdir2() and an internal VFS inode_operation mkdir2() to allow
    passing of a flag argument.
    Allow Samba to specify that it wants the created directory to inherit the
    unmodified ACL from its parent.
    
    Note: This is a hack to tide us over until we have proper support in the
    upstream kernel for this kind of system call.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -941,4 +941,6 @@ asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
 
+/* Primary Data */
+asmlinkage long sys_mkdirat2(int dfd, const char __user * pathname, umode_t mode, unsigned int flags);
 #endif
