738c59bbdda3f5f4ec05050a3ae9e9da5f20d8eb
Trond Myklebust <trond.myklebust@primarydata.com>
2018-03-29 11:32:29 -0400

    NFSv4: Detect support for OPEN4_SHARE_ACCESS_WANT_OPEN_XOR_DELEGATION
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>

--- include/linux/nfs_fs_sb.h
+++ include/linux/nfs_fs_sb.h
@@ -248,6 +248,7 @@ struct nfs_server {
 #define NFS_CAP_LGOPEN		(1U << 5)
 #define NFS_CAP_CASE_INSENSITIVE	(1U << 6)
 #define NFS_CAP_DELEGTIME	(1U << 7)
+#define NFS_CAP_OPEN_XOR	(1U << 8)
 #define NFS_CAP_POSIX_LOCK	(1U << 14)
 #define NFS_CAP_UIDGID_NOMAP	(1U << 15)
 #define NFS_CAP_STATEID_NFSV41	(1U << 16)
