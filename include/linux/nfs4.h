3c643a13db6abeee7556a33f87158bdc870d90c4
Trond Myklebust <trond.myklebust@primarydata.com>
2018-03-29 11:22:21 -0400

    NFSv4: Add support for the FATTR4_OPEN_ARGUMENTS attribute
    Query the server for the OPEN arguments that it supports so that
    we can figure out which extensions we can use.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Signed-off-by: Lance Shelton <lance.shelton@hammerspace.com>

--- include/linux/nfs4.h
+++ include/linux/nfs4.h
@@ -427,6 +427,7 @@ enum lock_type4 {
 #define FATTR4_WORD2_OFFLINE		(1UL << 19)
 #define FATTR4_WORD2_TIME_DELEG_ACCESS	(1UL << 20)
 #define FATTR4_WORD2_TIME_DELEG_MODIFY	(1UL << 21)
+#define FATTR4_WORD2_OPEN_ARGUMENTS	(1UL << 22)
 
 /* MDS threshold bitmap bits */
 #define THRESHOLD_RD                    (1UL << 0)
