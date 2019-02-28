f184ce8abc28ee7d671245cbf8bc868273bdca6b
Trond Myklebust <trond.myklebust@hammerspace.com>
2019-02-28 10:34:13 -0500

    NFS/flexfiles: Simplify nfs4_ff_layout_select_ds_fh()
    Pass in a pointer to the mirror rather than having to retrieve it from
    the array and then verify the resulting pointer.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/flexfilelayout/flexfilelayout.c
+++ fs/nfs/flexfilelayout/flexfilelayout.c
@@ -1847,7 +1847,7 @@ ff_layout_read_pagelist(struct nfs_pageio_descriptor *desc,
 	hdr->pgio_done_cb = ff_layout_read_done_cb;
 	refcount_inc(&ds->ds_clp->cl_count);
 	hdr->ds_clp = ds->ds_clp;
-	fh = nfs4_ff_layout_select_ds_fh(lseg, idx);
+	fh = nfs4_ff_layout_select_ds_fh(mirror);
 	if (fh)
 		hdr->args.fh = fh;
 	/*
@@ -1928,7 +1928,7 @@ ff_layout_write_pagelist(struct nfs_pageio_descriptor *desc,
 	refcount_inc(&ds->ds_clp->cl_count);
 	hdr->ds_clp = ds->ds_clp;
 	hdr->ds_commit_idx = idx;
-	fh = nfs4_ff_layout_select_ds_fh(lseg, idx);
+	fh = nfs4_ff_layout_select_ds_fh(mirror);
 	if (fh)
 		hdr->args.fh = fh;
 
