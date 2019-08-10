1cd64b872722650ba07212d831128e015843ef46
Trond Myklebust <trond.myklebust@hammerspace.com>
2019-08-10 15:10:29 -0400

    nfsd: Don't automount when looking up export paths
    Don't automount paths when looking up exports.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfsd/export.c
+++ fs/nfsd/export.c
@@ -143,7 +143,7 @@ static int expkey_parse(struct cache_detail *cd, char *mesg, int mlen)
 		if (!ek)
 			err = -ENOMEM;
 	} else {
-		err = kern_path(buf, LOOKUP_AUTOMOUNT, &key.ek_path);
+		err = kern_path(buf, 0, &key.ek_path);
 		if (err) {
 			/*
 			 * Ignore ETIMEDOUT/EAGAIN and just flush the
@@ -577,7 +577,7 @@ static int svc_export_parse(struct cache_detail *cd, char *mesg, int mlen)
 	if ((len = qword_get(&mesg, buf, PAGE_SIZE)) <= 0)
 		goto out1;
 
-	err = kern_path(buf, LOOKUP_AUTOMOUNT, &exp.ex_path);
+	err = kern_path(buf, 0, &exp.ex_path);
 	if (err) {
 		/*
 		 * Ignore ETIMEDOUT/EAGAIN and just flush the
@@ -932,7 +932,7 @@ exp_rootfh(struct net *net, struct auth_domain *clp, char *name,
 
 	err = -EPERM;
 	/* NB: we probably ought to check that it's NUL-terminated */
-	if (kern_path(name, LOOKUP_AUTOMOUNT, &path)) {
+	if (kern_path(name, 0, &path)) {
 		printk("nfsd: exp_rootfh path not found %s", name);
 		return err;
 	}
