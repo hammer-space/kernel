e92c33b170a333629b7cb9fa155bf81edd7d1efa
Trond Myklebust <trond.myklebust@hammerspace.com>
2020-10-30 17:29:12 -0400

    NFS: Remove unnecessary inode locking in nfs_llseek_dir()
    Remove the contentious inode lock, and instead provide thread safety
    using the file->f_lock spinlock.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -948,7 +948,6 @@ EXPORT_SYMBOL_GPL(nfs_readdir);
 
 loff_t nfs_llseek_dir(struct file *filp, loff_t offset, int whence)
 {
-	struct inode *inode = file_inode(filp);
 	struct nfs_open_dir_context *dir_ctx = filp->private_data;
 
 	dfprintk(FILE, "NFS: llseek dir(%pD2, %lld, %d)\n",
@@ -960,15 +959,15 @@ loff_t nfs_llseek_dir(struct file *filp, loff_t offset, int whence)
 	case SEEK_SET:
 		if (offset < 0)
 			return -EINVAL;
-		inode_lock(inode);
+		spin_lock(&filp->f_lock);
 		break;
 	case SEEK_CUR:
 		if (offset == 0)
 			return filp->f_pos;
-		inode_lock(inode);
+		spin_lock(&filp->f_lock);
 		offset += filp->f_pos;
 		if (offset < 0) {
-			inode_unlock(inode);
+			spin_unlock(&filp->f_lock);
 			return -EINVAL;
 		}
 	}
@@ -980,7 +979,7 @@ loff_t nfs_llseek_dir(struct file *filp, loff_t offset, int whence)
 			dir_ctx->dir_cookie = 0;
 		dir_ctx->duped = 0;
 	}
-	inode_unlock(inode);
+	spin_unlock(&filp->f_lock);
 	return offset;
 }
 EXPORT_SYMBOL_GPL(nfs_llseek_dir);
