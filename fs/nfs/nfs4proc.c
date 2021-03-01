655ea1e1f4c5bb4b88d45195f661b784ed0333fb
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-03-01 17:52:41 -0500

    richacl: Don't lie about the existence of an ACL
    Don't have nfs4_xattr_get_richacl() claim there is no ACL just because
    it doesn't match the richacl designer's idea of what it should look
    like.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -7686,19 +7686,13 @@ static int nfs4_xattr_get_richacl(const struct xattr_handler *handler,
 {
 	struct richacl *acl;
 	int error;
-	umode_t mode = inode->i_mode & S_IFMT;
 
 	acl = nfs4_proc_get_acl(inode);
 	if (IS_ERR(acl))
 		return PTR_ERR(acl);
 	if (acl == NULL)
 		return -ENODATA;
-	error = -ENODATA;
-	if (richacl_equiv_mode(acl, &mode) == 0 &&
-	    ((mode ^ inode->i_mode) & S_IRWXUGO) == 0)
-		goto out;
 	error = richacl_to_xattr(&init_user_ns, acl, buf, buflen);
-out:
 	richacl_put(acl);
 	return error;
 }
