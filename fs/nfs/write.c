06e7b01009eaef8f1995ad50b61744b54e29a1a4
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-03-25 21:07:21 -0400

    NFS: Replace use of NFS_INO_REVAL_PAGECACHE when checking cache validity
    When checking cache validity, be more specific than just 'we want to
    check the page cache validity'. In almost all cases, we want to check
    that change attribute, and possibly also the size.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/write.c
+++ fs/nfs/write.c
@@ -1278,7 +1278,8 @@ static bool nfs_write_pageuptodate(struct page *page, struct inode *inode)
 
 	if (nfs_have_delegated_attributes(inode))
 		goto out;
-	if (nfsi->cache_validity & NFS_INO_REVAL_PAGECACHE)
+	if (nfsi->cache_validity &
+	    (NFS_INO_INVALID_CHANGE | NFS_INO_INVALID_SIZE))
 		return false;
 	smp_rmb();
 	if (test_bit(NFS_INO_INVALIDATING, &nfsi->flags))
