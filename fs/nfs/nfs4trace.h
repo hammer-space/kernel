529e020f726a0306f105b29a993ed1e52292a1b1
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-04-01 13:54:19 -0400

    NFSv4: Convert nfs_xdr_status tracepoint to an event class
    We would like the ability to record other XDR errors, particularly
    those that are due to server bugs.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/nfs4trace.h
+++ fs/nfs/nfs4trace.h
@@ -664,7 +664,7 @@ TRACE_EVENT(nfs4_xdr_bad_operation,
 		)
 );
 
-TRACE_EVENT(nfs4_xdr_status,
+DECLARE_EVENT_CLASS(nfs4_xdr_event,
 		TP_PROTO(
 			const struct xdr_stream *xdr,
 			u32 op,
@@ -699,6 +699,15 @@ TRACE_EVENT(nfs4_xdr_status,
 			__entry->op
 		)
 );
+#define DEFINE_NFS4_XDR_EVENT(name) \
+	DEFINE_EVENT(nfs4_xdr_event, name, \
+			TP_PROTO( \
+				const struct xdr_stream *xdr, \
+				u32 op, \
+				u32 error \
+			), \
+			TP_ARGS(xdr, op, error))
+DEFINE_NFS4_XDR_EVENT(nfs4_xdr_status);
 
 DECLARE_EVENT_CLASS(nfs4_open_event,
 		TP_PROTO(
