f545734f950beb1f2bcf024efeeb004286f5f454
Trond Myklebust <trond.myklebust@hammerspace.com>
2021-07-09 18:15:11 -0400

    NFSv4: Add an ioctl to allow retrieval of the NFS raw ACCESS mask
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/internal.h
+++ fs/nfs/internal.h
@@ -411,6 +411,8 @@ int nfs_mknod(struct user_namespace *, struct inode *, struct dentry *, umode_t,
 	      dev_t);
 int nfs_rename(struct user_namespace *, struct inode *, struct dentry *,
 	       struct inode *, struct dentry *, unsigned int);
+int nfs_get_access(struct inode *inode, const struct cred *cred,
+		   struct nfs_access_entry *cache, bool may_block);
 
 #ifdef CONFIG_NFS_V4_2
 static inline __u32 nfs_access_xattr_mask(const struct nfs_server *server)
