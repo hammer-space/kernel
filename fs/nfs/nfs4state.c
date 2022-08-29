472ffd926fdbb384468a52a3afbcf161637b6125
Trond Myklebust <trond.myklebust@hammerspace.com>
2022-08-29 18:04:29 -0400

    NFSv4/pNFS: Do layout state recovery upon reboot
    Some pNFS implementations, such as flexible files, want the client to
    send the layout stats and layout errors that may have incurred while the
    metadata server was booting. To do so, the client sends a layoutreturn
    with an all-zero stateid while the server is in grace during reboot
    recovery.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>

--- fs/nfs/nfs4state.c
+++ fs/nfs/nfs4state.c
@@ -2034,7 +2034,6 @@ static int nfs4_establish_lease(struct nfs_client *clp)
 	put_cred(cred);
 	if (status != 0)
 		return status;
-	pnfs_destroy_all_layouts(clp);
 	return 0;
 }
 
@@ -2648,6 +2647,8 @@ static void nfs4_state_manager(struct nfs_client *clp)
 			section = "reclaim reboot";
 			status = nfs4_do_reclaim(clp,
 				clp->cl_mvops->reboot_recovery_ops);
+			if (status == 0)
+				status = pnfs_layout_handle_reboot(clp);
 			if (status == -EAGAIN)
 				continue;
 			if (status < 0)
@@ -2672,6 +2673,7 @@ static void nfs4_state_manager(struct nfs_client *clp)
 				continue;
 			if (status < 0)
 				goto out_error;
+			pnfs_destroy_all_layouts(clp);
 			clear_bit(NFS4CLNT_RECLAIM_NOGRACE, &clp->cl_state);
 		}
 
