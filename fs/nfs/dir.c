524a227c10a2285b37476b2ced610b868cc8bdd2
Benjamin Coddington <bcodding@redhat.com>
2022-09-20 13:00:21 -0400

    NFS: Trigger the "ls -l" readdir heuristic sooner
    Since commit 1a34c8c9a49e ("NFS: Support larger readdir buffers") has
    updated dtsize, and with recent improvements to the READDIRPLUS helper
    heuristic, the heuristic may not trigger until many dentries are emitted
    to userspace.   This will cause many thousands of GETATTR calls for "ls
    -l" when the directory's pagecache has already been populated.  This
    manifests as poor performance for long directory listings after an
    initially fast "ls -l".
    
    Fix this by emitting only 17 entries for any first pass through the NFS
    directory's ->iterate_shared(), which allows userpace to prime the
    counters for the heuristic.
    
    Signed-off-by: Benjamin Coddington <bcodding@redhat.com>
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    (cherry picked from commit 85aa8ddc3818718208c3cfdfda9c8c908c9dead1)
    Signed-off-by: Mike Snitzer <snitzer@hammerspace.com>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -1066,6 +1066,8 @@ static int readdir_search_pagecache(struct nfs_readdir_descriptor *desc)
 	return res;
 }
 
+#define NFS_READDIR_CACHE_MISS_THRESHOLD (16UL)
+
 /*
  * Once we've found the start of the dirent within a page: fill 'er up...
  */
@@ -1075,6 +1077,7 @@ static void nfs_do_filldir(struct nfs_readdir_descriptor *desc,
 	struct file	*file = desc->file;
 	struct nfs_cache_array *array;
 	unsigned int i;
+	bool first_emit = !desc->dir_cookie;
 
 	array = kmap(desc->page);
 	for (i = desc->cache_entry_index; i < array->size; i++) {
@@ -1098,6 +1101,10 @@ static void nfs_do_filldir(struct nfs_readdir_descriptor *desc,
 			desc->ctx->pos = desc->dir_cookie;
 		else
 			desc->ctx->pos++;
+		if (first_emit && i > NFS_READDIR_CACHE_MISS_THRESHOLD + 1) {
+			desc->eob = true;
+			break;
+		}
 	}
 	if (array->page_is_eof)
 		desc->eof = !desc->eob;
@@ -1178,8 +1185,6 @@ out:
 	dfprintk(DIRCACHE, "NFS: %s: returns %d\n", __func__, status);
 	return status;
 }
-
-#define NFS_READDIR_CACHE_MISS_THRESHOLD (16UL)
 
 static bool nfs_readdir_handle_cache_misses(struct inode *inode,
 					    struct nfs_readdir_descriptor *desc,
