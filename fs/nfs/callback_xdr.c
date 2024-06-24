0ef175dcd4615dc5c5d8fd9a6d11526c0779aeeb
Mike Snitzer <snitzer@hammerspace.com>
2024-06-24 05:28:54 +0000

    SUNRPC: Use per-CPU counters to tally server RPC counts
    commit 65ba3d2425bf51165b6e88509c632bd15d12883d
    Author: Chuck Lever <chuck.lever@oracle.com>
    Date:   Tue Jan 10 10:31:54 2023 -0500
    
        SUNRPC: Use per-CPU counters to tally server RPC counts
        
         - Improves counting accuracy
         - Reduces cross-CPU memory traffic
        
        Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/nfs/callback_xdr.c
+++ fs/nfs/callback_xdr.c
@@ -1109,7 +1109,8 @@ static const struct svc_procedure nfs4_callback_procedures1[] = {
 	}
 };
 
-static unsigned int nfs4_callback_count1[ARRAY_SIZE(nfs4_callback_procedures1)];
+static DEFINE_PER_CPU_ALIGNED(unsigned long,
+			      nfs4_callback_count1[ARRAY_SIZE(nfs4_callback_procedures1)]);
 const struct svc_version nfs4_callback_version1 = {
 	.vs_vers = 1,
 	.vs_nproc = ARRAY_SIZE(nfs4_callback_procedures1),
@@ -1121,7 +1122,8 @@ const struct svc_version nfs4_callback_version1 = {
 	.vs_need_cong_ctrl = true,
 };
 
-static unsigned int nfs4_callback_count4[ARRAY_SIZE(nfs4_callback_procedures1)];
+static DEFINE_PER_CPU_ALIGNED(unsigned long,
+			      nfs4_callback_count4[ARRAY_SIZE(nfs4_callback_procedures1)]);
 const struct svc_version nfs4_callback_version4 = {
 	.vs_vers = 4,
 	.vs_nproc = ARRAY_SIZE(nfs4_callback_procedures1),
