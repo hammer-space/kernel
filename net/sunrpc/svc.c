9d576f30f0acb085e5f6de3f030a8d32be382277
Mike Snitzer <snitzer@hammerspace.com>
2024-06-24 05:28:56 +0000

    SUNRPC: Ignore return value of ->xpo_sendto
    commit 55fcc7d9159de886296626e47db2c81f8578c7e1
    Author: Chuck Lever <chuck.lever@oracle.com>
    Date:   Mon Apr 3 13:53:07 2023 -0400
    
        SUNRPC: Ignore return value of ->xpo_sendto
        
        Clean up: All callers of svc_process() ignore its return value, so
        svc_process() can safely be converted to return void. Ditto for
        svc_send().
        
        The return value of ->xpo_sendto() is now used only as part of a
        trace event.
        
        Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- net/sunrpc/svc.c
+++ net/sunrpc/svc.c
@@ -1456,11 +1456,12 @@ err_system_err:
 	goto sendit;
 }
 
-/*
- * Process the RPC request.
+/**
+ * svc_process - Execute one RPC transaction
+ * @rqstp: RPC transaction context
+ *
  */
-int
-svc_process(struct svc_rqst *rqstp)
+void svc_process(struct svc_rqst *rqstp)
 {
 	struct kvec		*resv = &rqstp->rq_res.head[0];
 	__be32 *p;
@@ -1496,7 +1497,8 @@ svc_process(struct svc_rqst *rqstp)
 
 	if (!svc_process_common(rqstp))
 		goto out_drop;
-	return svc_send(rqstp);
+	svc_send(rqstp);
+	return;
 
 out_baddir:
 	svc_printk(rqstp, "bad direction 0x%08x, dropping request\n",
@@ -1505,7 +1507,6 @@ out_baddir:
 		rqstp->rq_server->sv_stats->rpcbadfmt++;
 out_drop:
 	svc_drop(rqstp);
-	return 0;
 }
 EXPORT_SYMBOL_GPL(svc_process);
 
