a1225247221a65adea3f7c94eaabd669617ca2be
Mike Snitzer <snitzer@hammerspace.com>
2024-06-24 05:29:07 +0000

    SUNRPC: add list of idle threads
    Hand-patched svc_rqst_alloc().
    
    commit 5ff817b23534dd3942f881ab01dd5050505517aa
    Author: NeilBrown <neilb@suse.de>
    Date:   Mon Sep 11 10:39:11 2023 -0400
    
        SUNRPC: add list of idle threads
        
        Rather than searching a list of threads to find an idle one, having a
        list of idle threads allows an idle thread to be found immediately.
        
        This adds some spin_lock calls which is not ideal, but as the hold-time
        is tiny it is still faster than searching a list.  A future patch will
        remove them using llist.h.  This involves some subtlety and so is left
        to a separate patch.
        
        This removes the need for the RQ_BUSY flag.  The rqst is "busy"
        precisely when it is not on the "idle" list.
        
        Signed-off-by: NeilBrown <neilb@suse.de>
        Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- net/sunrpc/svc.c
+++ net/sunrpc/svc.c
@@ -510,6 +510,7 @@ __svc_create(struct svc_program *prog, struct svc_stat *stats,
 		pool->sp_id = i;
 		lwq_init(&pool->sp_xprts);
 		INIT_LIST_HEAD(&pool->sp_all_threads);
+		INIT_LIST_HEAD(&pool->sp_idle_threads);
 		spin_lock_init(&pool->sp_lock);
 
 		percpu_counter_init(&pool->sp_messages_arrived, 0, GFP_KERNEL);
@@ -652,7 +653,7 @@ svc_rqst_alloc(struct svc_serv *serv, struct svc_pool *pool, int node)
 
 	pagevec_init(&rqstp->rq_pvec);
 
-	__set_bit(RQ_BUSY, &rqstp->rq_flags);
+	svc_thread_set_busy(rqstp);
 	spin_lock_init(&rqstp->rq_lock);
 	rqstp->rq_server = serv;
 	rqstp->rq_pool = pool;
@@ -714,10 +715,13 @@ void svc_pool_wake_idle_thread(struct svc_pool *pool)
 	struct svc_rqst	*rqstp;
 
 	rcu_read_lock();
-	list_for_each_entry_rcu(rqstp, &pool->sp_all_threads, rq_all) {
-		if (test_and_set_bit(RQ_BUSY, &rqstp->rq_flags))
-			continue;
-
+	spin_lock_bh(&pool->sp_lock);
+	rqstp = list_first_entry_or_null(&pool->sp_idle_threads,
+					 struct svc_rqst, rq_idle);
+	if (rqstp)
+		list_del_init(&rqstp->rq_idle);
+	spin_unlock_bh(&pool->sp_lock);
+	if (rqstp) {
 		WRITE_ONCE(rqstp->rq_qtime, ktime_get());
 		wake_up_process(rqstp->rq_task);
 		rcu_read_unlock();
