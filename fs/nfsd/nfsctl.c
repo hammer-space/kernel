654b8ff3a6ae2aa68ae0714bb6c1d7c2266bab02
Mike Snitzer <snitzer@hammerspace.com>
2024-11-08 21:56:12 +0000

    nfs_common: track all open nfsd_files per LOCALIO nfs_client
    snitzer/nfs-localio-for-next (for 6.13) commit 6863bd211b44d07e91b01feb55e5b192d4c39836
    Author: Mike Snitzer <snitzer@kernel.org>
    Date:   Mon Oct 28 13:11:50 2024 -0500
    
        nfs_common: track all open nfsd_files per LOCALIO nfs_client
        
        This tracking enables __nfsd_file_cache_purge() to call
        nfs_localio_invalidate_clients(), upon shutdown or export change, to
        nfs_close_local_fh() all open nfsd_files that are still cached by the
        LOCALIO nfs clients associated with nfsd_net that is being shutdown.
        
        Now that the client must track all open nfsd_files there was more work
        than necessary being done with the global nfs_uuids_lock contended.
        This manifested in various RCU issues, e.g.:
         hrtimer: interrupt took 47969440 ns
         rcu: INFO: rcu_sched detected stalls on CPUs/tasks:
        
        Use nfs_uuid->lock to protect all nfs_uuid_t members, instead of
        nfs_uuids_lock, once nfs_uuid_is_local() adds the client to
        nn->local_clients.
        
        Also add 'local_clients_lock' to 'struct nfsd_net' to protect
        nn->local_clients.  And store a pointer to spinlock in the 'list_lock'
        member of nfs_uuid_t so nfs_localio_disable_client() can use it to
        avoid taking the global nfs_uuids_lock.
        
        In combination, these split out locks eliminate the use of the single
        nfslocalio.c global nfs_uuids_lock in the IO paths (open and close).
        
        Also refactored associated fs/nfs_common/nfslocalio.c methods' locking
        to reduce work performed with spinlocks held in general.
        
        Signed-off-by: Mike Snitzer <snitzer@kernel.org>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -1488,6 +1488,7 @@ static __net_init int nfsd_net_init(struct net *net)
 	seqlock_init(&nn->writeverf_lock);
 	nfsd_proc_stat_init(net);
 #if IS_ENABLED(CONFIG_NFS_LOCALIO)
+	spin_lock_init(&nn->local_clients_lock);
 	INIT_LIST_HEAD(&nn->local_clients);
 #endif
 	return 0;
@@ -1512,7 +1513,8 @@ static __net_exit void nfsd_net_pre_exit(struct net *net)
 {
 	struct nfsd_net *nn = net_generic(net, nfsd_net_id);
 
-	nfs_localio_invalidate_clients(&nn->local_clients);
+	nfs_localio_invalidate_clients(&nn->local_clients,
+				       &nn->local_clients_lock);
 }
 #endif
 
